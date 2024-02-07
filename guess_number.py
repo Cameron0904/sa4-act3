5637f2544db67e0aca489442deab28d3948e8c50
Cameron Cox <cameronacox0904@gmail.com>
2024-02-06 18:36:07 -0600

    Added the feature of multiple guesses until they guess the number or enter 'q'

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,14 @@
 number = 10
-
+guess = 0
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+while guess != "q":
+    guess = (input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print(f"Sorry that was incorrect. Try again!")
+
+if guess == 'q':
+    print(f'The number was {number}')
