8566cc3afb09ecebf973b0952634e567bf4a8bb9
Cameron Cox <cameronacox0904@gmail.com>
2024-02-06 19:12:28 -0600

    Fixed indentation errors and a broken if statement that were not caught on the first commit after merge

--- guess_number.py
+++ guess_number.py
@@ -2,24 +2,31 @@ number = 10
 guess = 0
 print("I'm thinking of a number...")
 count = 5
-while guess != "q":
+
+while True:
+    
     if count == 0:
         print(f'The number was {number}')
         break
+    
     guess = (input("What number am I thinking of? "))
+
+    if guess == 'q':
+        break
 
     if int(guess) == number:
         print("Congratulations! You guessed the right number.")
         break
-    elif guess == 'q':
+    
     else:
+        
         print(f"Sorry that was incorrect. Try again!")
+        
         if int(guess) > number:
             print("The number is lower(hint)!")
         else:
             print("The number is higher(hint)!")
-
-
+        
         count -=1
         print(f'You have {count} guesses left!')
 
